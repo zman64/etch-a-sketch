dc840d896c0ea6a1d21f719f1055191ce7edd60f
Zachary Kahlig <zkahlig@AmpPartners.Org>
2022-05-13 15:45:16 -0400

    Arrange Divs in container using flexbox
    correctly size divs using flexbox and layout structure of project
    by changing body properties.
    Add button at top to get input for number of squares

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const container = document.querySelector('#container')
 for(let i=0; i < 16; i++){
     let div = document.createElement('div');
-
+    div.style.backgroundColor = 'red'
+    let width = container.clientWidth / 2;
+    div.style.width = `${width}px`
+    div.classList.add('flex-grow-1','boxes')
+    container.appendChild(div)
+    div.addEventListener('mouseenter', (e) => {
+        div.style.backgroundColor = 'blue'
+    })
 }
+
+const button = document.querySelector('#button')
+
+button.addEventListener('click', () => {
+    let number = prompt('Number of Square per Side?')
+})
+
